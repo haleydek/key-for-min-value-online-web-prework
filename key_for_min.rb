654f0b439398906932e3cef8f5152d38fbab439d
Haley DeKeyser <hcd09@live.com>
2019-05-12 20:32:02 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,17 +3,32 @@
 
 def key_for_min_value(name_hash)
   
-  name_hash_values = []
-  
+  hash_values = []
+
   name_hash.each do |key, value|
-    name_hash_values << value
+    value
+    hash_values << value
   end
-  
-  name_hash_values.each_with_index do |value, i|
-    while i < name_hash_values.size
-      if value < name_hash_values[i += 1]
-        value
+
+  hash_values
+
+  smallest = hash_values[0]
+
+  hash_values.each do |value|
+    i = 1
+    while i < hash_values.size
+      if value < smallest
+        smallest = value
       end
+    i += 1
     end
   end
+
+  smallest
+
+name_hash.key(smallest)
+
 end
+
+
+key_for_min_value(hash)
